12014340b75b4d54b81a09788f798944caad2bfb
Nguyen Hiep <hiephap1705@gmail.com>
2023-06-17 01:01:33 +0700

    complete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,5 @@
 import ToDoModel from '../model/Todo.js'
 
-
 const addToDo = async (req, res) => {
     try {
         const newTodo = await ToDoModel.create({
@@ -17,12 +16,46 @@ const gettAllToDos = async (req, res) => {
     try {
         // Lấy ra tất cả todo và sắp xếp theo thức tự mới nhất lên đầu
         const allToDos = await ToDoModel.find({
-        }).sort({createdAt:-1})
-        
+        }).sort({ createdAt: -1 })
+
         return res.status(200).json(allToDos)
     } catch (error) {
         return res.status(500).json(error.message)
     }
 
 }
-export { addToDo,gettAllToDos};
+const toggleToDoDone = async (req, res) => {
+    try {
+        console.log("Toggle API",req.body)
+        // Lấy ra tất cả todo và sắp xếp theo thức tự mới nhất lên đầu
+        const todoRef = await ToDoModel.findById(req.params.id)
+        const todo = await ToDoModel.findByIdAndUpdate({_id:req.params.id},{done:!todoRef.done})
+        await todo.save();
+        return res.status(200).json(todo)
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
+const updateTodo = async (req,res) => {
+    try {
+        console.log('BackEnd ....',req.body)
+        await ToDoModel.findOneAndUpdate({_id:req.params.id},{data:req.body.data})
+
+        // ko can save vi update already save
+        // await todo.save();
+        // tra ve cho frontend
+        const todo = await ToDoModel.findById(req.params.id)
+        return res.status(200).json(todo)
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
+const deleteTodo = async (req,res) => {
+    try {
+        const todo =   await ToDoModel.findByIdAndDelete(req.params.id)
+        return res.status(200).json(todo)
+    } catch (error) {
+        return res.status(500).json(error.message)
+    }
+}
+export { addToDo, gettAllToDos, toggleToDoDone,updateTodo,deleteTodo };
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { addToDo,gettAllToDos } from '../controllers/todoController.js';
+import { addToDo,gettAllToDos,toggleToDoDone,updateTodo,deleteTodo } from '../controllers/todoController.js';
 
 const route = express.Router();
 
 route.post('/todos',addToDo)
 route.get('/todos',gettAllToDos)
-
+route.get('/todos/:id',toggleToDoDone)
+route.put('/todos/:id',updateTodo)
+route.delete('/todos/:id',deleteTodo)
 export default route;
